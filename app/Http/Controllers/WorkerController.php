a2e492bb9938853a69b27e696e5aa6d7a72bc204
venom <ruslantelenkov@mail.ru>
2023-09-27 15:19:06 +0700

    test2

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,15 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\Polzovatel;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class WorkerController extends Controller
 {
    public function create(Request $request){
-       //dd($request);
+       $this->validationData($request);
+        $polzovatel = [
+            'name' => $request['name'],
+            'phone-number' => $request['phone-number'],
+            'date' => $request['date'],
+            'sms' => $request['sms'],
+            'email' => $request['email'],
+            'skype' => $request['skype'],
+            'status' => $request['status'],
+            'functionality' => $request['functionality'],
+            'prior' => $request['prior'],
+            'phone' => $request['phone'],
+        ];
+        $worker = Polzovatel::create($polzovatel);
+        $conpany = Company::where('name', $request['company'])->get();
+        $worker->companies()->attach($conpany);
+
+            dd('created');
    }
+
    public function show(){
-      // echo phpinfo();
-       return view('create-new-worker-form');
+       $conpanies = Company::all();
+
+       return view('create-new-worker-form',['companies' => $conpanies]);
+
+   }
+
+    /**
+     * @throws ValidationException
+     */
+    public function validationData(Request $request){
+
+       $this->validate($request,[
+           'name' => 'required|string|max:40',
+           'phone-number' =>'numeric|max_digits:10',
+           'email'=>'max:30',
+           'skype'=>'max:30',
+           'status'=>'required|max:30',
+           'functionality'=>'max:50',
+           'prior'=>'required|integer|max_digits:3',
+           'phone'=>'min_digits:3|max_digits:3'
+       ], [
+           'name.required' =>'Введите инициалы нового сотрудника',
+           'name.string' =>'Инициалы сотрудника не должны содержать цифры',
+           'name.max_digits' =>'Превышено максимальное колличество символов',
+
+           'phone-number.numeric' =>'Мобильный телефон не должен содержать буквы',
+           'phone-number.max_digits' =>'Превышено максимальное колличество символов',
+
+           'email.max_digits'=>'Превышено максимальное колличество символов',
+
+           'skype.max_digits'=>'Превышено максимальное колличество символов',
+
+           'status.required' =>'Введите должность нового сотрудника',
+           'status.max_digits'=>'Превышено максимальное колличество символов',
+
+           'functionality.max_digits'=>'Превышено максимальное колличество символов',
+
+           'prior.required' =>'Введите приоритет',
+           'prior.integer' => 'Приоритет должен являться численным значением',
+           'prior.max_digits' => 'Превышено максимальное колличество символов',
+
+           'phone.min_digits' =>'Превышено минимальное колличество символов',
+           'phone.max_digits' =>'Превышено максимальное колличество символов',
+       ]);
    }
 }
--- resources/views/create-new-worker-form.blade.php
+++ resources/views/create-new-worker-form.blade.php
@@ -1,46 +1,86 @@
 <!DOCTYPE html>
-dgsfgf
-
-sdfgfsg
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @section('title-admin') Admin-form @endsection
-
-    <h1> Test form </h1>
-
     <form action="{{route('new-worker-create')}}" method="POST">
         @csrf
-        <h1>Регистрация</h1>
+        <h1>Форма создания сотрудника</h1>
+
         <div><label> FIO
         <input type="text" name="name" id="name" placeholder="name of worker"/>
         </label></div>
+        @error('name')
+        {{$message}}
+        @enderror
         <div>
         <label> phone number
             <input type="text" name="phone-number" id="phone-number" placeholder="phone-number"/>
         </label></div>
+        @error('phone-number')
+        {{$message}}
+        @enderror
         <div>
         <label>SMS
             <input type="checkbox" id="sms" name="sms checked /">
         </label></div>
+        @error('sms')
+        {{$message}}
+        @enderror
+        <div>
 
-<div>
-    <label>
-        <input type="date" name="date" id="date" placeholder="date" value="2018-07-22" min="1990-01-01"/>
-    </label>
-</div>
-
-        <label>
-            <input type="text" name="name" id="name" placeholder="name of worker"/>
-        </label>
-        <label>
-            <input type="text" name="name" id="name" placeholder="name of worker"/>
-        </label>
-        <label>
-            <input type="text" name="name" id="name" placeholder="name of worker"/>
-        </label>
-        <label>
-            <input type="text" name="name" id="name" placeholder="name of worker"/>
-        </label>
-
+         <label> birthday
+          <input type="date" name="date" id="date" placeholder="date" value="2018-07-22" min="1990-01-01"/>
+         </label>
+        </div>
+        @error('date')
+        {{$message}}
+        @enderror
+        <div>
+        <label> EMAIL
+            <input type="email" name="email" id="email" placeholder="email"/>
+        </label></div>
+        @error('email')
+        {{$message}}
+        @enderror
+        <div>
+        <label> SKYPE
+            <input type="text" name="skype" id="skype" placeholder="skype"/>
+        </label></div>
+        @error('skype')
+        {{$message}}
+        @enderror
+        <div> <label> STATUS
+            <input type="text" name="status" id="status" placeholder="status"/>
+        </label></div>
+        @error('status')
+        {{$message}}
+        @enderror
+        <div> <label>FUNCTIONALITY
+            <input type="text" name="functionality" id="functionality" placeholder="functionality"/>
+        </label></div>
+        @error('functionality')
+        {{$message}}
+        @enderror
+        <div> <label>PRIOTITY
+                <input type="number" name="prior" id="prior" placeholder="prior"/>
+            </label></div>
+        @error('prior')
+        {{$message}}
+        @enderror
+        <div> <label>Phone
+                <input type="text" name="phone" id="phone" placeholder="phone"/>
+            </label>
+        </div>
+        @error('phone')
+        {{$message}}
+        @enderror
+        <div>
+            <select name="company" id="company">
+            @foreach($companies as $company)
+                    <option>{{$company->name}}</option>
+            @endforeach
+            </select>
+        </div>
+    <input type="submit" value="Создать сотрудника" />
     </form>
 
 </html>
